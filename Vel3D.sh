ff03e66e00f1b2718eb03ac080d2c18f52ee7ba3
rcheng15jhu <rcheng15@jh.edu>
2021-10-18 16:56:03 -0400

    Added job script and renamed Vel3D

---
+++ Vel3D.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH --job-name=Vel3D
+#SBATCH --time=50:00:00
+#SBATCH --partition=a100
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=48
+#SBATCH --gres=gpu:4
+#SBATCH -A rni2_gpu
+#SBATCH --mail-type=end
+#SBATCH --mail-user=rcheng15@jhu.edu
+
+source /data/apps/go.sh
+ml anaconda
+conda activate HFMtf1.15
+cd /home/rcheng15/HFM/HFM_fork/Source 
+python3.7 Vel3D.py
